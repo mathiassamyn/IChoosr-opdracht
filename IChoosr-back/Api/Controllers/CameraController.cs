70890245e5db3cfa94717c0b66c85898abf7eb8a
Mathias Samyn <mathiassamyn@Mathiass-MBP.lan>
2021-01-21 10:26:32 +0100

    remove unnecessary template data

---
+++ IChoosr-back/Api/Controllers/CameraController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CameraController : ControllerBase
+    {
+        
+
+        public CameraController()
+        {
+        }
+
+        //[HttpGet]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+
+        //}
+    }
+}
